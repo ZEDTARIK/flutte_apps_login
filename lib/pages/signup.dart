01d097e6762b901c4c0f6579c8289d32693a45d2
Zouhair <ettarak.zouhair@gmail.com>
2022-08-08 18:37:04 +0000

    Add pictures in page Singup

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Signup extends StatelessWidget {
   const Signup({Key? key}) : super(key: key);
@@ -13,13 +16,21 @@ class Signup extends StatelessWidget {
           color: Colors.amber,
           child: Stack(
             children: [
-              Column(
-                children: const [
-                  Text(
-                    "Signup",
-                    style: TextStyle(fontSize: 20, fontFamily: "myFont"),
-                  ),
-                ],
+              SizedBox(
+                width: double.infinity,
+                height: double.infinity,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 35,
+                    ),
+                    Text(
+                      "Signup",
+                      style: TextStyle(fontSize: 35, fontFamily: "myFont"),
+                    ),
+                    SvgPicture.asset("assets/icons/signup.svg")
+                  ],
+                ),
               ),
               Positioned(
                   child: Image.asset(
